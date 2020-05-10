e0d3d1a70ecc9b6540fa6c215182290f7fd2669d
jesus munoz <jmunoz@omnibnk.com>
2020-05-10 16:27:12 -0500

    fix urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,11 +21,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Weezle API",
+      title="Django Template API",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="jmunoz@weezle.co"),
+      contact=openapi.Contact(email="tech@django-template.co"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
